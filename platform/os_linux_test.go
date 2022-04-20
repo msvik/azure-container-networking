e83cb82b31d4b38e4e716678e55cce10179bdf7d
VK <misvik@users.noreply.github.com>
2022-04-20 13:07:57 -0700

    Added unit test for ExecuteCommand for linux

---
+++ platform/os_linux_test.go
@@ -0,0 +1,24 @@
+package platform
+
+import (
+	"testing"
+)
+
+// Default timeout in ExecuteCommand is 10 seconds so test should fail with 12 seconds execution time
+func TestExecuteCommandTimeout(t *testing.T) {
+	client := NewExecClient()
+	_, err := client.ExecuteCommand("sleep 12")
+	if err == nil {
+		t.Errorf("TestExecuteCommandTimeout should have returned timeout error")
+	}
+	t.Logf("%s", err.Error())
+}
+
+// Default timeout in ExecuteCommand is 10 seconds so test should pass with 8 seconds execution time
+func TestExecuteCommandNoTimeout(t *testing.T) {
+	client := NewExecClient()
+	_, err := client.ExecuteCommand("sleep 8")
+	if err != nil {
+		t.Errorf("TestExecuteCommandNoTimeout failed with error %v", err)
+	}
+}
