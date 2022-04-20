2ab8bf86b807a96a347ad86d0a211dbc456b21a4
VK <misvik@users.noreply.github.com>
2022-04-20 14:21:56 -0700

    Moved timeout to execlient. It has default timeout and method to override it now

--- platform/os_linux.go
+++ platform/os_linux.go
@@ -51,7 +51,7 @@ func GetOSInfo() string {
 }
 
 func GetProcessSupport() error {
-	p := &execClient{}
+	p := NewExecClient()
 	cmd := fmt.Sprintf("ps -p %v -o comm=", os.Getpid())
 	_, err := p.ExecuteCommand(cmd)
 	return err
@@ -84,8 +84,7 @@ func (p *execClient) ExecuteCommand(command string) (string, error) {
 	var out bytes.Buffer
 
 	// Create a new context and add a timeout to it
-	const timeoutSeconds = 10
-	ctx, cancel := context.WithTimeout(context.Background(), timeoutSeconds*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), p.Timeout)
 	defer cancel() // The cancel should be deferred so resources are cleaned up
 
 	cmd := exec.CommandContext(ctx, "sh", "-c", command)
@@ -101,7 +100,7 @@ func (p *execClient) ExecuteCommand(command string) (string, error) {
 }
 
 func SetOutboundSNAT(subnet string) error {
-	p := execClient{}
+	p := NewExecClient()
 	cmd := fmt.Sprintf("iptables -t nat -A POSTROUTING -m iprange ! --dst-range 168.63.129.16 -m addrtype ! --dst-type local ! -d %v -j MASQUERADE",
 		subnet)
 	_, err := p.ExecuteCommand(cmd)
@@ -119,7 +118,7 @@ func ClearNetworkConfiguration() (bool, error) {
 }
 
 func KillProcessByName(processName string) error {
-	p := &execClient{}
+	p := NewExecClient()
 	cmd := fmt.Sprintf("pkill -f %v", processName)
 	_, err := p.ExecuteCommand(cmd)
 	return err
@@ -150,7 +149,7 @@ func GetOSDetails() (map[string]string, error) {
 }
 
 func GetProcessNameByID(pidstr string) (string, error) {
-	p := &execClient{}
+	p := NewExecClient()
 	pidstr = strings.Trim(pidstr, "\n")
 	cmd := fmt.Sprintf("ps -p %s -o comm=", pidstr)
 	out, err := p.ExecuteCommand(cmd)
@@ -166,7 +165,7 @@ func GetProcessNameByID(pidstr string) (string, error) {
 }
 
 func PrintDependencyPackageDetails() {
-	p := &execClient{}
+	p := NewExecClient()
 	out, err := p.ExecuteCommand("iptables --version")
 	out = strings.TrimSuffix(out, "\n")
 	log.Printf("[cni-net] iptable version:%s, err:%v", out, err)
--- platform/os_linux_test.go
+++ platform/os_linux_test.go
@@ -2,11 +2,14 @@ package platform
 
 import (
 	"testing"
+	"time"
 )
 
 // Default timeout in ExecuteCommand is 10 seconds so test should fail with 12 seconds execution time
 func TestExecuteCommandTimeout(t *testing.T) {
-	client := NewExecClient()
+	const timeout = 10 * time.Second
+	client := NewExecClientTimeout(timeout)
+
 	_, err := client.ExecuteCommand("sleep 12")
 	if err == nil {
 		t.Errorf("TestExecuteCommandTimeout should have returned timeout error")
@@ -16,7 +19,9 @@ func TestExecuteCommandTimeout(t *testing.T) {
 
 // Default timeout in ExecuteCommand is 10 seconds so test should pass with 8 seconds execution time
 func TestExecuteCommandNoTimeout(t *testing.T) {
-	client := NewExecClient()
+	const timeout = 10 * time.Second
+	client := NewExecClientTimeout(timeout)
+
 	_, err := client.ExecuteCommand("sleep 8")
 	if err != nil {
 		t.Errorf("TestExecuteCommandNoTimeout failed with error %v", err)
